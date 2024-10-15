cac505c188947ed932c6f0b68a28f3231f38da39
Caitlyn Nelson <caitlynnelson@u.boisestate.edu>
2024-10-15 15:20:41 -0600

    Project 9 Complete, Fully Functinal

--- README.md
+++ README.md
@@ -2,23 +2,21 @@
 
 ## Overview
 
-TODO: Write a project description
+The Evil Cirle with Bouncing Balls
+We wanted to make it more interactive by adding a user-controlled evil circle, which will eat the balls if it catches them. we create a generic Shape() object that our balls and evil circle can inherit from. Then we added a score counter to track the number of balls left to capture.
+
+How to Run the Code:
+1: Clone the repository into an IDE of your choice. Find the clone your repository button or copy and paste the repository link into the space for the url. Then find a place in your system to stored the copied repo. 
+
+2: Open the folder and display the project. You will see all files accompanying the project. You'll want to click on the one with the .html extension. Once that is open in your window, and assuming that you have Live Preview installed (*instructions for Live Preview at the end) Click the split rectangle with a little magnifying glass on it in the upper right corner to the left of the ... . This will open your Live Preview to view the website created.
+
+3: For the full effect of the design, please click on the three horizontal lines in the upper left hand corner of the screen and you'll find an open in browser option. Click on that to open the full website in your browser for review. Or you can open your viewing screen to its maximum width on your screen.
+
+*Search for the Live Preview feature in the extensions area of your IDE. Search for Live Preview and if not installed, install. Then to use either open it as laid out above or go to your view button and hit the command palette. Here you can search for Live Preview and click on it to start it up.
 
 ## Sources and Credits
 
-TODO: You must credit the sources and authors of any code, libraries, or other
-assets you use in your project. If you leave this section blank, your project
-will be considered in violation of the Academic Honesty policy unless you truly
-created everything from scratch with no outside help. If you need to use a
-source that you cannot credit (e.g. a classmate's work), you must get explicit
-permission from your instructor.
+I used the links and resources that were provided by the Professor for this project. I found the StrokeSytle and Stroke links to be pretty helpful.
 
-A simple bulleted list below is sufficient. For example:
-
-- Bootstrap: https://getbootstrap.com/
-- jQuery: https://jquery.com/
-- Background image: https://unsplash.com/photos/...
-- Sound effects: https://freesound.org/people/...
-- Icons: https://fontawesome.com/
-- Fonts: https://fonts.google.com/
-- etc.
+- StrokeStyle: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/strokeStyle
+- Stroke: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/stroke
--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@ const ctx = canvas.getContext("2d");
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
+//reference to paragraph
+const p = document.querySelector("p");
+let count = 0;
+
 // function to generate random number
 
 function random(min, max) {
@@ -26,6 +30,75 @@ class Shape {
     this.velX = velX;
     this.velY = velY;
   }
+}
+
+//Evil Circle Class
+class EvilCircle extends Shape{
+  constructor(x, y){
+    super(x, y, 20, 20);
+    this.color = "white";
+    this.size = 10;
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    });
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = 3;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+
+  checkBounds() {
+    if (this.x + this.size >= width) {
+      this.x = -Math.abs(this.x);
+    }
+
+    if (this.x - this.size <= 0) {
+      this.x = Math.abs(this.x);
+    }
+
+    if (this.y + this.size >= height) {
+      this.y = -Math.abs(this.y);
+    }
+
+    if (this.y - this.size <= 0) {
+      this.y = Math.abs(this.y);
+    }
+  }
+
+  collisionDetect() {
+    //detect collision with the evilcircle
+      for (const ball of balls) {
+        if (ball.exists) {
+          const dx = this.x - ball.x;
+          const dy = this.y - ball.y;
+          const distance = Math.sqrt(dx * dx + dy * dy);
+
+          if (distance < this.size + ball.size) {
+            ball.exists = false;
+            count--;
+            p.textContent = 'Ball Count: ' + count;
+          }
+        }
+      }
+  }
+
 }
 
 class Ball extends Shape{
@@ -71,16 +144,14 @@ class Ball extends Shape{
 
   collisionDetect() {
     //detect collision only is the exists value is true
-    if(this.exists == true) {
-      for (const ball of balls) {
-        if (!(this === ball)) {
-          const dx = this.x - ball.x;
-          const dy = this.y - ball.y;
-          const distance = Math.sqrt(dx * dx + dy * dy);
+    for (const ball of balls) {
+      if (!(this === ball) && ball.exists == true) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
 
-          if (distance < this.size + ball.size) {
-            ball.color = this.color = randomRGB();
-          }
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
         }
       }
     }
@@ -113,16 +184,28 @@ while (balls.length < 25) {
   );
 
   balls.push(ball);
+  count++;
+  p.textContent = 'Ball Count: ' + count;
 }
+
+const evil = new EvilCircle(random(0, width), random(0, height));
 
 function loop() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
   ctx.fillRect(0, 0, width, height);
 
+
   for (const ball of balls) {
-    ball.draw();
-    ball.update();
-    ball.collisionDetect();
+    if(ball.exists) {
+      ball.draw();
+      ball.update();
+      ball.collisionDetect();
+    }
+
+    evil.draw();
+    evil.checkBounds();
+    evil.collisionDetect();
+
   }
 
   requestAnimationFrame(loop);
